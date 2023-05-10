90fbb28a98745d76851b1dc5fba07df9b80db621
welton-2020 <weltdias@gmail.com>
2023-05-10 00:00:20 -0300

    Create entity, service,controller,repository - Game

---
+++ src/test/java/com/wsl/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsl.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
